5890fbde2d813265db578f797149a47728d71671
web developer <43838771+vivoX9@users.noreply.github.com>
2023-07-22 23:04:14 +0800

    去除敏感信息

--- config/constant.ts
+++ config/constant.ts
@@ -10,21 +10,21 @@ export const FIXED_KEY = {
 export const DATABASE = {
   development: {
     dbName: 'server',
-    user: 'root',
-    password: '8322640.wjj',
+    user: '',
+    password: '',
     host: 'localhost',
     port: 3306
   },
   production: {
     dbName: 'server',
-    user: 'root',
-    password: '8322640.wjj',
+    user: '',
+    password: '',
     host: 'localhost',
     port: 3306
   }
 }
 
 export const JWT = {
-  secret: 'secret',
+  secret: '',
   expires: 60 * 60 * 24 * 30 // 30天
 }
